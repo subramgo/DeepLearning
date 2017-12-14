66fafa6e1c8b790fc86c78695989dfcc6e6627fe
Gopi Subramanian <gsubramanian@tycoint.com>
2017-12-14 15:48:46 -0500

    option to have 16 dimensional y variable added

--- python/scripts/img-process/adience-clean-label.py
+++ python/scripts/img-process/adience-clean-label.py
@@ -17,6 +17,17 @@ image_path  = '../data/Adience/faces/'
 age_filter = {'(0, 2)' :0, '(4, 6)':1 , '(8, 12)':2, '(15, 20)':3,'(25, 32)':4,'(38, 43)':5, '(48, 53)':6, '(60, 100)':7}
 gen_filter = {'m':0,'f':1}
 
+age_gender_filter = {
+	'f-(0, 2)' :0,'m-(0, 2)' :1, 
+           'f-(4, 6)':2 ,'m-(4, 6)':3 , 
+           'f-(8, 12)':4, 'm-(8, 12)':5, 
+           'f-(15, 20)':6,'m-(15, 20)':7,
+           'f-(25, 32)':8,'m-(25, 32)':9,
+           'f-(38, 43)':10, 'm-(38, 43)':11, 
+           'f-(48, 53)':12, 'm-(48, 53)':13, 
+           'f-(60, 100)':14,'m-(60, 100)':15
+           }
+
 
 
 labels_files = glob.glob(labels_path)
@@ -52,10 +63,12 @@ labels_df['image_loc'] = image_path + labels_df['user_id'] + '/' +  labels_df['i
 # Get the corresponding class integer from filter dict
 labels_df['gender_class'] = labels_df['gender'].apply(lambda x: gen_filter[x])
 labels_df['age_class']     = labels_df['age'].apply(lambda x: age_filter[x]) 
+labels_df['all_class']       = labels_df.apply(lambda row:  age_gender_filter[ row['gender'] + '-' + row['age'] ] , axis = 1)  
+
 
 
 # Save the new labels files
-labels_df[['user_id', 'original_image','face_id','age','gender','image_loc','age_class','gender_class']].to_csv(fnl_labels_path)
+labels_df[['user_id', 'original_image','face_id','age','gender','image_loc','age_class','gender_class','all_class']].to_csv(fnl_labels_path)
 
 print("After Pre-processing")
 print_stats(labels_df)
