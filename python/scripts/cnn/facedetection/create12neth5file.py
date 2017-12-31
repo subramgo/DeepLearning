dba864deb154b825ef2ef09dd9fea1e6c1b76a70
Gopi Subramanian <gsubramanian@tycoint.com>
2017-12-31 10:22:57 -0500

    create hdf5 for facenet12

---
+++ python/scripts/cnn/facedetection/create12neth5file.py
@@ -0,0 +1,68 @@
+
+import glob
+import pandas as pd
+import os
+import cv2
+import numpy as np
+import h5py
+
+
+
+# Labels folder
+csv_file_path = '../data/facedetection/final.csv'
+hdf5_path = '../data/facedetection/12net.h5'
+
+labels_df = pd.read_csv(csv_file_path)
+train_df = labels_df[labels_df['type'] == 'train']
+eval_df = labels_df[labels_df['type'] == 'eval'] 
+
+total_images = labels_df.shape[0] 
+eval_images = eval_df.shape[0]
+train_images = train_df.shape[0]
+
+print("Train {} Eval {} Total {}".format(train_images, eval_images, total_images))
+
+############### h5py file creation #####################
+train_shape = (train_images, 15, 15, 3)
+eval_shape  = (eval_images, 15, 15, 3)
+
+# Open a hdf5 file and create earray
+hdf5_file = h5py.File(hdf5_path, mode = 'w')
+
+hdf5_file.create_dataset("train_images", train_shape, np.int8)
+hdf5_file.create_dataset("train_labels", (train_images,), np.int8)
+
+hdf5_file.create_dataset("eval_images", eval_shape, np.int8)
+hdf5_file.create_dataset("eval_labels", (eval_images,), np.int8)
+
+hdf5_file["train_labels"][...] = train_df['class']
+hdf5_file["eval_labels"][...] =  eval_df['class']
+
+
+
+images_location_list = labels_df['directory'].tolist()
+type_list = labels_df['type'].tolist()
+## Load train image
+i = 0
+train_i = 0
+eval_i = 0
+for (_type, path) in zip(type_list, images_location_list):
+    if i % 1000 == 0 and i > 0:
+        print("Train data {}/{}".format(i, total_images))
+    
+    image = cv2.imread(path, cv2.IMREAD_COLOR)
+    resized_image = cv2.resize(image, (15, 15)) 
+    if _type == 'train':
+    	hdf5_file["train_images"][train_i, ...] = resized_image[None]
+    	train_i+=1
+
+    if _type == 'eval':
+    	hdf5_file["eval_images"][eval_i, ...] = resized_image[None]
+    	eval_i+=1
+
+    i+=1
+
+hdf5_file.close()
+
+print("Finished creating hdf5 file.")
+
