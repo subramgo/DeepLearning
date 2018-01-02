418870c955e61d1a8cfa592e36bf33bdb9279b37
Gopi Subramanian <gsubramanian@tycoint.com>
2018-01-02 11:04:26 -0500

    consider only jpg images

--- python/scripts/cnn/facedetection/vgg16/createLabelCsv.py
+++ python/scripts/cnn/facedetection/vgg16/createLabelCsv.py
@@ -2,7 +2,7 @@ import os
 import numpy as np 
 
 
-csv_file_path = '../data/facedetection/final.csv'
+csv_file_path = '../data/facedetection/vgg/final.csv'
 face_path = '../data/Adience/faces'
 nonface_path = '../data/cifar-10/train/'
 
@@ -14,11 +14,11 @@ f = open(csv_file_path, 'w')
 f.write("type,class,directory\n")
 
 type_ = ""
-class_ = "1" # face
+class_ = "1" # faces
 directory_= ""
 for subdir, dirs, files in os.walk(face_path):
     for file in files:
-        if file.endswith('.jpg') or file.endswith('.png'):
+        if file.endswith('.jpg'):
 	        directory_ = os.path.join(subdir, file)
 	        if np.random.rand() > eval_percentage:
 	        	type_ = "train"
@@ -29,7 +29,7 @@ for subdir, dirs, files in os.walk(face_path):
 class_ = "0" # No face
 for subdir, dirs, files in os.walk(nonface_path):
     for file in files:
-        if file.endswith('.jpg') or file.endswith('.png'):
+        if file.endswith('.jpg'):
 	        directory_ = os.path.join(subdir, file)
 	        if np.random.rand() > eval_percentage:
 	        	type_ = "train"
